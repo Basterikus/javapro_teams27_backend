7400116a80a9e0cd1e855e6b4d1ef1d02afaf3a7
Алексей Коняев <alexey.konyaev.ru@yandex.ru>
2022-08-10 22:37:30 +0400

    Merge 'dev' into 'aleksei'

---
+++ src/main/java/org/javaproteam27/socialnetwork/security/LoginController.java
@@ -0,0 +1,31 @@
+package org.javaproteam27.socialnetwork.security;
+
+import lombok.RequiredArgsConstructor;
+import org.javaproteam27.socialnetwork.security.jwt.JwtTokenProvider;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+    private final JdbcTemplate jdbcTemplate;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping("/api/v1/auth/login")
+    public String getToken(){
+        AvailabilityUser availabilityUser = new AvailabilityUser(jdbcTemplate);
+        String email = "alex@mail.com";
+        if (availabilityUser.checkUser(email)){
+            System.out.println("true");
+            return jwtTokenProvider.createToken(email);
+        }
+        return "it's not ok";
+    }
+
+    @GetMapping("/api/v1/test")
+    public String test(){
+        return "ok";
+    }
+
+}

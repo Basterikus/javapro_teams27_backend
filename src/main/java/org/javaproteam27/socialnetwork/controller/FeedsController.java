45e41c45cd23d96cc59b1b7fa6339629bf4fde7d
Антон Макров <makrov_an@mail.ru>
2022-08-12 23:29:35 +0800

    Rename FeedsController done!

--- src/main/java/org/javaproteam27/socialnetwork/controller/FeedsController.java
+++ src/main/java/org/javaproteam27/socialnetwork/controller/FeedsController.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1/feeds")
 @RequiredArgsConstructor
-public class FeedsRestApiController {
+public class FeedsController {
     private final PostService postService;
 
     @GetMapping("")

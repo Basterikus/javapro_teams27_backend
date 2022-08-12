7d21a19eb3320c3293276301a0f47a7cc0c667a0
andrei_mashukov <JCVNN7e!!m85FDt>
2022-08-12 14:25:29 +0700

    Переделал систему входа с учетом новых данных

--- src/main/java/org/javaproteam27/socialnetwork/handler/LoginExceptionHandler.java
+++ src/main/java/org/javaproteam27/socialnetwork/handler/LoginExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.javaproteam27.socialnetwork.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.javaproteam27.socialnetwork.handler.exception.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +14,13 @@ public class LoginExceptionHandler {
     public ResponseEntity<InvalidRequestError> catchInvalidRequestException(InvalidRequestException e) {
         log.error(e.getMessage(), e);
         return new ResponseEntity<>(new InvalidRequestError("invalid_request", e.getMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<InvalidRequestError> catchEntityNotFoundException(EntityNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(new InvalidRequestError("invalid_request", "Incorrect email"),
                 HttpStatus.BAD_REQUEST);
     }
 }

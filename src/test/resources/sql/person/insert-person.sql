2f6255ea922ab8d50cd091402716c16777b38be9
andrei_mashukov <JCVNN7e!!m85FDt>
2022-10-03 17:30:14 +0700

    - Some fix from Sonar in LoggerAspect - DialogsController and DialogsService unit-tests added - Some fix in DialogsService and DialogsController

--- src/test/resources/sql/person/insert-person.sql
+++ src/test/resources/sql/person/insert-person.sql
@@ -2,3 +2,11 @@ INSERT INTO person(id, first_name, last_name, reg_date, birth_date, email, phone
 confirmation_code, is_approved, messages_permission, last_online_time, is_blocked)
     values (1, 'test', 'testov', '2011-01-01 00:00:00', '2011-01-01 00:00:00', 'test@mail.ru', '8950', 'test1234',
     'photo', 'about', 'Moscow', 'Russia', 1234, true, 'ALL', '2011-01-01 00:00:00', false);
+INSERT INTO person(id, first_name, last_name, reg_date, birth_date, email, phone, password, photo, about, city, country,
+confirmation_code, is_approved, messages_permission, last_online_time, is_blocked)
+    values (2, 'second', 'test', '2011-01-01 00:00:00', '2011-01-01 00:00:00', 'test2@mail.ru', '89506', 'test1234',
+    'photo', 'about', 'Moscow', 'Russia', 1234, true, 'ALL', '2011-01-01 00:00:00', false);
+INSERT INTO person(id, first_name, last_name, reg_date, birth_date, email, phone, password, photo, about, city, country,
+confirmation_code, is_approved, messages_permission, last_online_time, is_blocked)
+    values (3, 'third', 'test', '2011-01-01 00:00:00', '2011-01-01 00:00:00', 'test3@mail.ru', '895064', 'test1234',
+    'photo', 'about', 'Moscow', 'Russia', 1234, true, 'ALL', '2011-01-01 00:00:00', false);

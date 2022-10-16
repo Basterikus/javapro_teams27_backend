c494d23d41065479bae19dad636131ee4c6eb012
Mikhail <pks52@mail.ru>
2022-10-16 20:56:53 +0300

    add Redis

---
+++ src/main/java/org/javaproteam27/socialnetwork/config/RedisConfig.java
@@ -0,0 +1,54 @@
+package org.javaproteam27.socialnetwork.config;
+
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisConnection;
+import com.lambdaworks.redis.RedisURI;
+import lombok.RequiredArgsConstructor;
+import org.javaproteam27.socialnetwork.repository.PersonRepository;
+import org.javaproteam27.socialnetwork.util.DropBox;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private RedisClient redisClient;
+    private RedisConnection<String, String> connection;
+    private final PersonRepository personRepository;
+    private final DropBox dropBox;
+
+    private void init() {
+            redisClient = new RedisClient(
+                    RedisURI.create("redis://127.0.0.1:6379"));
+            connection = redisClient.connect();
+    }
+
+    private void add(Integer id, String url) {
+        connection.set(String.valueOf(id), url);
+    }
+
+    public String getUrl(String id) {
+        return connection.get(id);
+    }
+
+    @Scheduled(fixedDelayString = "PT24H")
+    @Async
+    private void updateUrl() {
+        if (connection == null) {
+            init();
+        }
+        System.out.println("Работает updateUrl");
+        personRepository.findAll().forEach(person ->
+            add(person.getId(), dropBox.getLinkImages(person.getPhoto())));
+        System.out.println("сработал updateUrl");
+    }
+
+    public void shutdown() {
+        connection.close();
+        redisClient.shutdown();
+    }
+
+}
